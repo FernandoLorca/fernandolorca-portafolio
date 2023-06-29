71eefd7e716c5de12c8800eda6894734a41c0e63
FernandoLorca <florcapinto@gmail.com>
2023-06-29 17:39:36 -0400

    add github and linkedin icons in header and in top of contact form

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import ContactForm from './components/mobileComponents/ContactForm/ContactForm';
 import MobileMenu from './components/mobileComponents/Navbar/MobileMenu';
 import TabletNavbar from './components/tabletComponents/tabletNavbar/TabletNavbar';
 import GoTop from './components/GoTop';
+import { BsGithub, BsLinkedin } from 'react-icons/bs';
 
 function App() {
   return (
@@ -24,12 +25,30 @@ function App() {
             <AboutMe />
             <Habilities />
             <Projects />
-            <div className="flex justify-center items-center py-8">
+            <div className="flex flex-col justify-center items-center py-16">
               <img
                 src="/logo-florca.svg"
                 alt="Logo Fernando Lorca"
                 className="w-24 h-24 md:w-32 md:h-32 lg:w-36 lg:h-36"
               />
+              <div className="flex gap-2">
+                <a
+                  href="https://github.com/FernandoLorca"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="w-10 h-10 flex justify-center items-center hover:opacity-75"
+                >
+                  <BsGithub className="text-2xl" />
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/fernandolorcapinto/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="w-10 h-10 flex justify-center items-center hover:opacity-75"
+                >
+                  <BsLinkedin className="text-2xl" />
+                </a>
+              </div>
             </div>
             <ContactForm />
             <div className="mt-10">
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,4 @@
+import { BsGithub, BsLinkedin } from 'react-icons/bs';
 import { useGlobalStateContext } from '../context/useGlobalStateContext';
 
 import Button from '../components/Button';
@@ -33,6 +34,24 @@ const Hero = () => {
         text="Descubre mis proyectos"
         link="#projects"
       />
+      <div className="flex mt-5 gap-5">
+        <a
+          href="https://github.com/FernandoLorca"
+          target="_blank"
+          rel="noreferrer"
+          className="w-10 h-10 flex justify-center items-center hover:opacity-75"
+        >
+          <BsGithub className="text-3xl" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/fernandolorcapinto/"
+          target="_blank"
+          rel="noreferrer"
+          className="w-10 h-10 flex justify-center items-center hover:opacity-75"
+        >
+          <BsLinkedin className="text-3xl" />
+        </a>
+      </div>
       <div className="h-64 flex items-center">
         <div className="w-3/4 horizontalBars bg-white"></div>
       </div>
